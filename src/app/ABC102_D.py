8e4ad6bb7df0f52b45dd71e0b9c94a631c2273ff
takus4649 <takus4649@gmail.com>
2018-07-03 05:05:44 +0900

    Finish ABC102.

--- src/app/ABC102_D.py
+++ src/app/ABC102_D.py
@@ -1,38 +1,31 @@
+from itertools import accumulate
+
+
 def run(n, a):
-    sum_a = [sum(a[0:i+1]) for i in range(n)]
-    ret = sum_a[n - 1]
-    for i in range(2, n-1):
-        j_l = search_j(sum_a[: i], 0)
-        j_r = search_j(sum_a[i:], sum_a[i - 1]) + i
-        p = sum_a[j_l]
-        q = sum_a[i - 1] - p
-        r = sum_a[j_r] - q - p
-        s = sum_a[n - 1] - r - q - p
-        diff = max(p, q, r, s) - min(p, q, r, s)
-        if ret > diff:
-            ret = diff
-    return ret
-
-
-def search_j(sum_a, before):
-    n = len(sum_a)
-    sum_l = sum_a[0] - before
-    sum_r = sum_a[n - 1] - sum_a[0]
-    diff1 = sum_r - sum_l
-    ret = 0
-    for i in range(n - 1):
-        sum_l = sum_a[i] - before
-        sum_r = sum_a[n - 1] - sum_a[i]
-        diff2 = sum_r - sum_l
-        if diff2 < 0:
-            if abs(diff1) < abs(diff2):
-                return i - 1
-            else:
-                return i
-        else:
-            diff1 = diff2
-            ret = i
-    return ret
+    # sum_a = [sum(a[0:i+1]) for i in range(n)]
+    sum_a = list(accumulate(a))
+    li = 0
+    ri = 2
+    t = sum_a[-1]
+    diff = sum_a[-1]
+    # 真ん中の切れ込みを順に変更
+    # 左の切れ込みの位置はiに対して単調増加
+    # 左の切れ込みで真ん中の項を移動するかどうかは、
+    # 真ん中の項を除いて、小さい方に入れる
+    # ただし単調増加なので、左の切れ込みの左側が
+    # 小さいときに真ん中を左の切れ込みの左側に入れる
+    # 右も同様に考える
+    for i in range(1, n-2):
+        while sum_a[i] > sum_a[li+1] + sum_a[li]:
+            li += 1
+        while t + sum_a[i] > sum_a[ri+1] + sum_a[ri]:
+            ri += 1
+        p = sum_a[li]
+        q = sum_a[i] - p
+        r = sum_a[ri] - q - p
+        s = t - sum_a[ri]
+        diff = min(diff, max(p, q, r, s) - min(p, q, r, s))
+    return diff
 
 
 def read_line():
