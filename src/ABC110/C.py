5b5d9d591a53c027cbd0541ae776015f2a8a8125
takus4649 <takus4649@gmail.com>
2018-09-24 00:21:17 +0900

    Add right answer for C.

--- src/ABC110/C.py
+++ src/ABC110/C.py
@@ -1,13 +1,36 @@
-import collections
+# import collections
 
 
 def run(s, t):
+    sc = {}
+    tc = {}
+    for i in 'abcdefghijklmnopqrstuvwxyz':
+        sc[i] = 0
+        tc[i] = 0
+    counter = 1
+    for ss in s:
+        if sc[ss] == 0:
+            sc[ss] = counter
+            counter += 1
+    counter = 1
+    for tt in t:
+        if tc[tt] == 0:
+            tc[tt] = counter
+            counter += 1
+    slist = [sc[ss] for ss in s]
+    tlist = [tc[tt] for tt in t]
+    if slist == tlist:
+        return 'Yes'
+    else:
+        return 'No'
+    '''
     ss = sorted(list(collections.Counter(s).values()))
     tt = sorted(list(collections.Counter(t).values()))
     if ss == tt:
         return 'Yes'
     else:
         return 'No'
+    '''
 
 
 def main():
--- src/ABC110/test_C.py
+++ src/ABC110/test_C.py
@@ -5,9 +5,10 @@ from C import run
 
 class TestSample(unittest.TestCase):
     def test_run(self):
-        self.assertEqual(run('azzel' ,'apple'), 'Yes')
-        self.assertEqual(run('chokudai' ,'redcoder'), 'No')
-        self.assertEqual(run('abcdefghijklmnopqrstuvwxyz' ,'ibyhqfrekavclxjstdwgpzmonu'), 'Yes')
+        self.assertEqual(run('azzel', 'apple'), 'Yes')
+        self.assertEqual(run('chokudai', 'redcoder'), 'No')
+        self.assertEqual(run('abcdefghijklmnopqrstuvwxyz', 'ibyhqfrekavclxjstdwgpzmonu'), 'Yes')
+        self.assertEqual(run('apple', 'alepp'), 'No')
 
 
 if __name__ == '__main__':
