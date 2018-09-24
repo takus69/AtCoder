d4bc2931b124b0d46e202244ea4036897f683e71
takus4649 <takus4649@gmail.com>
2018-09-24 12:48:06 +0900

    Add D

--- src/ABC110/D.py
+++ src/ABC110/D.py
@@ -1,57 +1,46 @@
-import math
-import sys
-import collections
-
-
 mod = 1000000007
-sys.setrecursionlimit(mod)
-fact = {1: 1}
 
 
+# 計算量O(m**0.5*nlog(mod))
 def run(n, m):
     # print('{}を{}個の数列で表現'.format(m, n))
     ans = 1
-    primes = []
+    # 計算量O(m**0.5)
     for i in range(2, m):
-        if m == 1:
-            break
+        # iの2乗以上は素数は最大で1つしかない
         if i*i > m:
             break
         if m % i == 0:
             cnt = 0
+            # 計算量O(logm)
             while m % i == 0:
                 cnt += 1
                 m //= i
-                primes.append(i)
+            # 計算量O(nlog(mod))
             ans *= comb(cnt+n-1, n-1)
             ans %= mod
-    # counts = collections.Counter(primes)
-    # print(counts)
-    # for (_, v) in counts.items():
-    #     ans *= comb(v+n-1, n-1)
-    #     ans %= mod
+    # iの2乗以上の素数がある場合は、その配置の場合をかける
     if m != 1:
         ans *= n
         ans %= mod
     return ans
 
 
+# 計算量O(nlog(mod))
 def comb(n, r):
     if r > (n-r):
         r = n-r
     mul = 1
     div = 1
+    # 計算量O(n)
     for i in range(r):
         mul *= n-i
         div *= i+1
         mul %= mod
         div %= mod
-    # mul = math.factorial(n) // math.factorial(n - r)
-    # div = math.factorial(r)
-    # mul = factorial(n) // factorial(n - r)
-    # div = factorial(r)
     mul %= mod
     div %= mod
+    # 計算量O(log(mod))
     return (mul * modpow(div, (mod-2))) % mod
 
 
@@ -65,6 +54,7 @@ def factorial(n):
 '''
 
 
+# 計算量O(log(p))
 def modpow(a, p):
     if p == 0:
         return 1
