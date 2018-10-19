e30631a94dd704eb28a6f08a29325dc06f8be457
takus4649 <takus4649@gmail.com>
2018-10-19 17:08:05 +0900

    AC ABC099 D.

--- src/ABC099/D.py
+++ src/ABC099/D.py
@@ -1,10 +1,27 @@
 def run(N, C, D, c):
     '''
     マス番号に応じて3色に塗る必要がある
-    (i+j)%3が0, 1, 2それぞれに対して、ある色に塗るコストを計算する
+    (i+j)%3が0, 1, 2それぞれに対して、何色が何個あるか計算しておく
+    ある色に塗るコストを計算する
     0, 1, 2それぞれのC色のコストのパターンができる
     上記コストに対してC色から3色を選択する全パターンのうち最小のコストを求める
     '''
+    color0 = {}
+    color1 = {}
+    color2 = {}
+    for k in range(C):
+        color0[k] = 0
+        color1[k] = 0
+        color2[k] = 0
+    for i in range(N):
+        for j in range(N):
+            color = c[i][j] - 1
+            if (i+j+2) % 3 == 0:
+                color0[color] += 1
+            elif (i+j+2) % 3 == 1:
+                color1[color] += 1
+            elif (i+j+2) % 3 == 2:
+                color2[color] += 1
     cost0 = {}
     cost1 = {}
     cost2 = {}
@@ -12,16 +29,11 @@ def run(N, C, D, c):
         cost0[k] = 0
         cost1[k] = 0
         cost2[k] = 0
-        for i in range(N):
-            for j in range(N):
-                color = c[i][j] - 1
-                cost = D[color][k]
-                if (i+j+2) % 3 == 0:
-                    cost0[k] += cost
-                elif (i+j+2) % 3 == 1:
-                    cost1[k] += cost
-                elif (i+j+2) % 3 == 2:
-                    cost2[k] += cost
+        for i in range(C):
+            cost = D[i][k]
+            cost0[k] += cost * color0[i] 
+            cost1[k] += cost * color1[i] 
+            cost2[k] += cost * color2[i] 
     ret = max(cost0.values()) + max(cost1.values()) + max(cost2.values())
     for i in range(C):
         for j in range(C):
