ac70f2a9ae179132ab926654fa1c5314e0057014
takus4649 <takus4649@gmail.com>
2018-10-30 16:20:58 +0900

    Add UnionFind to algorism. Not test yet.

---
+++ src/algo/UnionFind.py
@@ -0,0 +1,26 @@
+class UnionFind():
+    '''Union find(ランクなし)'''
+    def __init__(self, n):
+        self.par = []
+        for i in range(n):
+            self.par.append(i) 
+    
+    def root(self, x):
+        if self.par[x] == x:
+            return x
+        else:
+            # 経路圧縮
+            self.par[x] = self.root(self.par[x])
+            return self.par[x]
+
+    def unite(self, x, y):
+        x_root = self.root(self.par[x])
+        y_root = self.root(self.par[y])
+        if x_root == y_root:
+            pass
+        else:
+            self.par[x_root] = y_root
+
+    def same(self, x, y):
+        return self.root(x) == self.root(y)
+    
