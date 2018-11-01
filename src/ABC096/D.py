d90ec53a63777fb1c1c7b988ff127479b1ee3795
takus4649 <takus4649@gmail.com>
2018-11-01 12:30:28 +0900

    AC D on ABC096.

--- src/ABC096/D.py
+++ src/ABC096/D.py
@@ -1,15 +1,26 @@
 def run(N):
-    # 素数のリストを作成
-    p = [2]
-    for i in range(3, 55555):
-        flag = True
-        for pp in p:
-            if i % pp == 0:
-                flag = False
-                continue
-        if flag:
-            p.append(i)
-    return p
+    p_list = primes(555555)
+    # 選択した素数が、5で割って余りが1のもののみ選択すれば、必ず合成数になる
+    ret = []
+    for p in p_list:
+        if p % 5 == 1:
+            ret.append(p)
+        if len(ret) == N:
+            break
+    return ' '.join([str(p) for p in ret])
+
+
+def primes(n):
+    '''
+    エラトステネスの篩にて、素数のリストを作成
+    '''
+    is_prime = [True] * (n+1)
+    is_prime[0] = False
+    is_prime[1] = False
+    for i in range(2, n+1):
+        for j in range(i*2, n+1, i):
+            is_prime[j] = False
+    return [i for i in range(n+1) if is_prime[i]]
 
 
 def main():
--- src/ABC096/test_D.py
+++ src/ABC096/test_D.py
@@ -1,13 +1,17 @@
 import unittest
 
-from D import run
+from D import run, primes
 
 
 class TestSample(unittest.TestCase):
     def test_run(self):
-        self.assertEqual(run(5), '3 5 7 11 31')
-        self.assertEqual(run(6), '2 3 5 7 11 13')
-        self.assertEqual(run(7), '2 5 7 13 19 37 67 79')
+        self.assertEqual(run(5), '11 31 41 61 71')
+        self.assertEqual(run(6), '11 31 41 61 71 101')
+        self.assertEqual(run(7), '11 31 41 61 71 101 131')
+
+    def test_primes(self):
+        self.assertEqual(primes(5), [2, 3, 5])
+        self.assertEqual(primes(20), [2, 3, 5, 7, 11, 13, 17, 19])
 
 
 if __name__ == '__main__':
