c6b95d46a60e18206ecc189734651277859fc8d3
takus4649 <takus4649@gmail.com>
2019-01-26 15:28:09 +0900

    AC D for ABC116.

--- src/ABC116/D.py
+++ src/ABC116/D.py
@@ -9,21 +9,23 @@ def run(N, K, t, d):
     '''
     t_dic = dict(zip(range(N), t))
     d_dic = dict(zip(range(N), d))
-    d_dic_sorted = dict(sorted(d_dic.items(), key=lambda x: x[1], reverse=True))
-    d_sum = sum(list(d_dic_sorted.values())[:K])
+    d_dic_sorted = sorted(d_dic.items(), key=lambda x: x[1], reverse=True)
+    d_dic_sorted_i = [dd[0] for dd in d_dic_sorted]
+    d_dic_sorted_d = [dd[1] for dd in d_dic_sorted]
+    d_sum = sum(d_dic_sorted_d[:K])
     t_used = {}
     t_cnt = 0
     for i in t:
         t_used[i] = 0
-    for i in list(d_dic_sorted.keys())[:K]:
+    for i in d_dic_sorted_i[:K]:
         t_used[t_dic[i]] += 1
         if t_used[t_dic[i]] == 1:
             t_cnt += 1
     t_sum = t_cnt ** 2
     max_sum = d_sum + t_sum
-    del_list = list(d_dic_sorted.keys())[:K]
-    for i in list(d_dic_sorted.keys())[K:]:
-        di = d_dic_sorted[i]
+    del_list = d_dic_sorted_i[:K]
+    for i in d_dic_sorted_i[K:]:
+        di = d_dic[i]
         ti = t_dic[i]
         # ネタの種類が増えるように追加
         if t_used[ti] == 0:
@@ -35,7 +37,7 @@ def run(N, K, t, d):
             flg = True
             while len(del_list) > 0 and flg:
                 del_i = del_list.pop()
-                del_di = d_dic_sorted[del_i]
+                del_di = d_dic[del_i]
                 del_ti = t_dic[del_i]
                 if t_used[del_ti] > 1:
                     t_used[del_ti] -= 1
--- src/ABC116/test_D.py
+++ src/ABC116/test_D.py
@@ -6,6 +6,7 @@ from D import run
 class TestSample(unittest.TestCase):
     def test_run(self):
         self.assertEqual(run(5, 3, [1, 1, 2, 2, 3], [9, 7, 6, 5, 1]), 26)
+        self.assertEqual(run(5, 3, [3, 2, 2, 1, 1], [1, 5, 6, 7, 9]), 26)
         self.assertEqual(run(7, 4, [1, 2, 3, 4, 4, 4, 4],
                          [1, 1, 1, 6, 5, 5, 5]), 25)
         self.assertEqual(run(6, 5, [5, 2, 3, 6, 6, 4],
