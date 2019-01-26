ba414144a3339202bb5f50f245cc49fbaa72d169
takus4649 <takus4649@gmail.com>
2019-01-26 14:59:24 +0900

    WA D for ABC116.

--- src/ABC116/D.py
+++ src/ABC116/D.py
@@ -1,12 +1,55 @@
 def run(N, K, t, d):
-    return (N, K, t, d)
+    '''
+    おいしさ(d)の昇順でソートする O(NlogN)
+    おいしさが大きいもののK個の合計を取得 O(K)
+    K個のうちおいしさが少ないものから順にネタの種類が
+    増えるように変更していく O(K)
+    ネタの種類のうち最大の物の値とインデックスを取得しておく O(K)
+    ネタの種類を変えた合計のうち最大のものが答え
+    '''
+    t_dic = dict(zip(range(N), t))
+    d_dic = dict(zip(range(N), d))
+    d_dic_sorted = dict(sorted(d_dic.items(), key=lambda x: x[1], reverse=True))
+    d_sum = sum(list(d_dic_sorted.values())[:K])
+    t_used = {}
+    t_cnt = 0
+    for i in t:
+        t_used[i] = 0
+    for i in list(d_dic_sorted.keys())[:K]:
+        t_used[t_dic[i]] += 1
+        if t_used[t_dic[i]] == 1:
+            t_cnt += 1
+    t_sum = t_cnt ** 2
+    max_sum = d_sum + t_sum
+    del_list = list(d_dic_sorted.keys())[:K]
+    for i in list(d_dic_sorted.keys())[K:]:
+        di = d_dic_sorted[i]
+        ti = t_dic[i]
+        # ネタの種類が増えるように追加
+        if t_used[ti] == 0:
+            t_used[ti] += 1
+            t_cnt += 1
+            t_sum += 2*t_cnt - 1
+            d_sum += di
+            # ネタが重複しているものを削除
+            flg = True
+            while len(del_list) > 0 and flg:
+                del_i = del_list.pop()
+                del_di = d_dic_sorted[del_i]
+                del_ti = t_dic[del_i]
+                if t_used[del_ti] > 1:
+                    t_used[del_ti] -= 1
+                    d_sum -= del_di
+                    max_sum = max(max_sum, d_sum + t_sum)
+                    flg = False
+    return max_sum
 
 
 def main():
     N, K = map(int, input().split())
     t = []
     d = []
-    for i in range(N):
+    for _ in range(N):
         ti, di = map(int, input().split())
         t.append(ti)
         d.append(di)
--- src/ABC116/test_D.py
+++ src/ABC116/test_D.py
@@ -6,8 +6,11 @@ from D import run
 class TestSample(unittest.TestCase):
     def test_run(self):
         self.assertEqual(run(5, 3, [1, 1, 2, 2, 3], [9, 7, 6, 5, 1]), 26)
-        self.assertEqual(run(7, 4, [1, 2, 3, 4, 4, 4, 4], [1, 1, 1, 6, 5, 5, 5]), 25)
-        self.assertEqual(run(6, 5, [5, 2, 3, 6, 6, 4], [1000000000, 990000000, 980000000, 970000000, 960000000, 950000000]), 25)
+        self.assertEqual(run(7, 4, [1, 2, 3, 4, 4, 4, 4],
+                         [1, 1, 1, 6, 5, 5, 5]), 25)
+        self.assertEqual(run(6, 5, [5, 2, 3, 6, 6, 4],
+                         [1000000000, 990000000, 980000000,
+                          970000000, 960000000, 950000000]), 4900000016)
 
 
 if __name__ == '__main__':
