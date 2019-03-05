d7f7acec4a3719d888868e049f14fadc647285e9
takus4649 <takus4649@gmail.com>
2019-03-05 12:58:37 +0900

    C is WA.

--- src/AGC030/C.py
+++ src/AGC030/C.py
@@ -1,32 +1,38 @@
 def run(K):
+    n_max = 500
     '''
     K <= 500の場合は、n = Kとして1行につき1色を塗ればよい
         111
         222
         333
-    Kが4の倍数の場合は、n = K/2として1行につき2色を交互に塗ればよい
-        1212
-        3434
-        5656
-        7878
-    上記でi色をn+i色に塗り替えても条件を満たす
-        1212
-        3434
-        1616
-        7878
+    Kが4の倍数の場合は、n = K/2以下の塗り方ができる
+        1234
+        5678
+        3412
+        7856
+    上記でn+i+1色をi色に塗り替えても条件を満たす
+        1234
+        5178
+        3412
+        7851
     K = 1000の時は、n = 500で満たし、塗り替え操作で501～999まで表現できる
     '''
     n = 0
     trouts = []
-    if K <= 500:
+    if K <= n_max:
         for i in range(K):
             n = K
             trouts.append([i+1]*K)
     else:
-        change = 1000 - K
-        c = [i for i in range(1, 501)] + [i for i in range(1, change+1)] + [i for i in range(501+change, 1001)]
-        for i in range(500):
-            trouts.append([c[2*i], c[2*i+1]]*250)
+        change = n_max*2 - K
+        c1 = [i for i in range(1, n_max+1)]
+        c2 = [n_max+1] + [i for i in range(1, change+1)] + [i for i in range(n_max+2+change, n_max*2+1)]
+        for i in range(n_max):
+            shift = (i//2) * 2
+            if i % 2 == 0:
+                trouts.append(c1[shift:] + c1[:shift])
+            else:
+                trouts.append(c2[shift:] + c2[:shift])
     return (n, trouts)
 
 
