d4abbe03f435a6f07154b4fcdd729ad8bcc87b17
Takuya Saito <takus4649@gmail.com>
2022-12-15 04:48:18 +0900

    D AC

--- src/ABC281/D.py
+++ src/ABC281/D.py
@@ -5,13 +5,13 @@ a = list(map(int, input().split()))
 dp = [[[-1 for k in range(D)] for j in range(K+1)] for i in range(N+1)]
 dp[0][0][0] = 0
 for i in range(N):
-    for j in range(K+1):
+    for j in range(min(K+1, i+1)):
         for k in range(D):
             if dp[i][j][k] == -1:
                 continue
             # aiを選ばない場合
             dp[i+1][j][k] = max(dp[i+1][j][k], dp[i][j][k])
             # aiを選ぶ場合
-            if j != K:
+            if j < K:
                 dp[i+1][j+1][(k+a[i])%D] = max(dp[i+1][j+1][(k+a[i])%D], dp[i][j][k]+a[i])
 print(dp[N][K][0])
