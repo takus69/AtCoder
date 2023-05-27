8dc5ce8221fc29e2aa2ef6efec948f0c953f0af2
Takuya Saito <takus4649@gmail.com>
2023-05-27 23:01:30 +0900

    ABC303 D

--- src/ABC303/D.py
+++ src/ABC303/D.py
@@ -1,61 +1,18 @@
 X, Y, Z = map(int, input().split())
 S = input()
 
-A = []
-C = []
-
-now = S[0]
-cnt = 0
-cnt_A = 0
-cnt_a = 0
-for s in S:
-    if now != s:
-        A.append(now)
-        C.append(cnt)
-        if now == 'A':
-            cnt_A += cnt
-        else:
-            cnt_a += cnt
-        now = s
-        cnt = 1
+dp = {}
+dp[(0, 0)] = 0
+dp[(0, 1)] = Z
+for i in range(1, len(S)+1):
+    s = S[i-1]
+    if s == 'A':
+        caps = 1
+        not_caps = 0
     else:
-        cnt += 1
-A.append(now)
-C.append(cnt)
-if now == 'A':
-    cnt_A += cnt
-else:
-    cnt_a += cnt
-
-caps = False
-ans = 0
-for i in range(len(A)):
-    now = A[i]
-    cnt = C[i]
-    if now == 'A':
-        cnt_A -= cnt
-    else:
-        cnt_a -= cnt
-    print(caps, now, cnt, ans, cnt_A, cnt_a)
-    if (caps and now == 'A') or (not caps and now == 'a'):
-        if X*cnt == (Y*cnt + Z):
-            ans += X*cnt
-            if (cnt_A < cnt_a and X < Y) or (cnt_A > cnt_a and X > Y):
-                caps = not caps
-        elif X*cnt > (Y*cnt + Z):
-            caps = not caps
-            ans += Y*cnt + Z
-        else:
-            ans += X*cnt
-    else:
-        if (X*cnt + Z) == Y*cnt:
-            ans += Y*cnt
-            if (cnt_A < cnt_a and X > Y) or (cnt_A > cnt_a and X < Y):
-                caps = not caps
-        elif (X*cnt + Z) > Y*cnt:
-            ans += Y*cnt
-        else:
-            caps = not caps
-            ans += X*cnt + Z
-
-print(ans)
+        caps = 0
+        not_caps = 1
+    dp[(i, caps)] = min(dp[(i-1, caps)]+X, dp[(i-1, not_caps)]+Z+X)
+    dp[(i, not_caps)] = min(dp[(i-1, not_caps)]+Y, dp[(i-1, caps)]+Z+Y)
+#print(dp)
+print(min(dp[(len(S), 0)], dp[(len(S), 1)]))
