f79dcd547138fccef83735930b93e90ee048cda4
Takuya Saito <takus4649@gmail.com>
2023-08-11 23:43:21 +0900

    評価関数追加

--- src/AHC022/test_A.py
+++ src/AHC022/test_A.py
@@ -1,12 +1,13 @@
 import unittest
 from typing import List
+import math
 from A import Pos, Judge, Solver
 '''
 ToDo
 - 生成inputによるシミュレーション(Done)
 - output生成(ビジュアライザーに入力可能)(Done)
 - ファイル入出力(Done)
-- 評価関数
+- 評価関数(Done)
 '''
 
 class MockJudge(Judge):
@@ -16,12 +17,18 @@ class MockJudge(Judge):
         self.cnt = 0
         self.landing_pos = landing_pos
         self.out_f = out_f
+        self.landing_cost = 0
+        self.measure_cost = 0
 
     def set_temperature(self, temperature: List[List[int]]) -> None:
         super().set_temperature(temperature)
         self.temperature = temperature
         for row in temperature:
             self.out_f.write(' '.join(map(str, row))+'\n')
+        L = len(temperature)
+        for i in range(L):
+            for j in range(L):
+                self.landing_cost += (temperature[i][j] - temperature[(i+1)%L][j])**2 + (temperature[i][j] - temperature[i][(j+1)%L])**2
 
     def measure(self, i, y, x):
         print(f'{i} {y} {x}', flush=True)
@@ -32,6 +39,7 @@ class MockJudge(Judge):
         c = pos.x + x
         v = self.temperature[r][c] + self.f[self.cnt]
         self.out_f.write(f'# measure i={i} y={y} x={x}, value={v}\n')
+        self.measure_cost += 100 * (10 + abs(y) + abs(x))
         return v
     
     def answer(self, estimate: List[int]) -> None:
@@ -39,6 +47,19 @@ class MockJudge(Judge):
         self.out_f.write('-1 -1 -1\n')
         for e in estimate:
             self.out_f.write(str(e)+'\n')
+        self.estimate = estimate
+
+    def evaluate(self):
+        print(f'配置コスト: {self.landing_cost}')
+        print(f'計測時コスト: {self.measure_cost}')
+        N = len(self.A)
+        W = 0
+        for i in range(N):
+            if self.A[i] != self.estimate[i]:
+                W += 1
+        print(f'誤り数: {W}/{N}')
+        score = math.ceil((10**14 * (0.8**W)) / (self.landing_cost + self.measure_cost + 10**5))
+        print(f'得点: {score}')
         
 
 class TestA(unittest.TestCase):
@@ -58,9 +79,11 @@ class TestA(unittest.TestCase):
             f.append(int(in_f.readline()))
         in_f.close()
 
-        solver = Solver(L, N, S, landing_pos, MockJudge(A, f, landing_pos, out_f))
+        judge = MockJudge(A, f, landing_pos, out_f)
+        solver = Solver(L, N, S, landing_pos, judge)
         solver.solve()
         out_f.close()
+        judge.evaluate()
 
 
 if __name__ == '__main__':
