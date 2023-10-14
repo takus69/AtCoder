c5818edee5a6e0b2bef9b684bf5e854912febc20
Takuya Saito <takus4649@gmail.com>
2023-10-14 18:23:50 +0900

    v1: D個の集合のうちランダムで2つを選択し、大から小の集合に、要素を入れ替える(1,402,034,771)

--- src/AHC025/A.py
+++ src/AHC025/A.py
@@ -3,40 +3,83 @@ import random
 
 random.seed(0)
 
-# 初期設定
-N, D, Q = map(int, input().split())
-ans = [d for d in range(D)]
-ans += [random.randint(0, D-1) for _ in range(D, N)]
-print('#C', ' '.join(map(str, ans)))
-q = 0  # クエリの回数
+class Solver:
+    def __init__(self):
+        # 初期設定
+        self.N, self.D, self.Q = map(int, input().split())
+        self.ans = [d for d in range(self.D)]
+        self.ans += [random.randint(0, self.D-1) for _ in range(self.D, self.N)]
+        print('#C', ' '.join(map(str, self.ans)))
+        self.q_cnt = 0  # クエリの回数
 
-# 初期測定
-def measure_d(dl, dr):
-    nl = 0
-    nr = 0
-    l = []
-    r = []
-    for i in range(N):
-        d = ans[i]
-        if d == dl:
-            nl += 1
-            l.append(i)
-        elif d == dr:
-            nr += 1
-            r.append(i)
-    print(' '.join(map(str, [nl, nr] + l + r)))
-    q = input()
-    print('# {} {} {}'.format(dl, q, dr))
-    return q
- 
-for d in range(D-1):
-    q = measure_d(d, d+1)
+    # 関数定義
+    def measure_d(self, dl, dr):
+        '''
+        D個の集合同士の比較
+        '''
+        nl = 0
+        nr = 0
+        l = []
+        r = []
+        for i in range(self.N):
+            d = self.ans[i]
+            if d == dl:
+                nl += 1
+                l.append(i)
+            elif d == dr:
+                nr += 1
+                r.append(i)
+        print(' '.join(map(str, [nl, nr] + l + r)))
+        q = input()
+        print('# {} {} {}'.format(dl, q, dr))
+        self.q_cnt += 1
+        return q
+
+    def measure_n(self, i1, i2):
+        print('1 1 {} {}'.format(i1, i2))
+        q = input()
+        print('# {} {} {}'.format(i1, q, i2))
+        self.q_cnt += 1
+        return q
+
+    def switch(self, i1, i2):
+        d1 = self.ans[i1]
+        d2 = self.ans[i2]
+        self.ans[i1] = d2
+        self.ans[i2] = d1
+
+    def solve(self):
+        while self.q_cnt < self.Q:
+            # Dの集合の比較
+            dl = random.randint(0, self.D-1)
+            dr = dl
+            while dr == dl:
+                dr = random.randint(0, self.D-1)
+            q_d = self.measure_d(dl, dr)
+            if self.q_cnt >= self.Q:
+                break
+            if q_d == '=':
+                continue
+            
+            # 要素の比較
+            dl_n = []
+            dr_n = []
+            for j in range(self.N):
+                if self.ans[j] == dl:
+                    dl_n.append(j)
+                elif self.ans[j] == dr:
+                    dr_n.append(j)
+            nl = random.choice(dl_n)
+            nr = random.choice(dr_n)
+            q_n = self.measure_n(nl, nr)
+            if q_d == q_n:
+                self.switch(nl, nr)
+            print('#C', ' '.join(map(str, self.ans)))
+
+        print(' '.join(map(str, self.ans)))
 
 
-D_cnt = {d: 0 for d in range(D)}
-for d in ans:
-    D_cnt[d] += 1
+if __name__ == '__main__':
+    solver = Solver()
+    solver.solve()
 
-for i in range(Q):
-   print('#C', ' '.join(map(str, ans)))
-print(' '.join(map(str, ans)))
