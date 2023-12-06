49c7c91746f287bb577066e01d91f2f91abf705c
Takuya Saito <takus4649@gmail.com>
2023-12-07 05:51:53 +0900

    結果出力して、良い悪いを確認する

--- src/AHC027/simulator.py
+++ src/AHC027/simulator.py
@@ -8,18 +8,21 @@ import pandas as pd
 def run(in_file, out_file):
     solver = MockSolver(in_file, out_file)
     score = solver.solve()
-    return score
+    return score, solver.N, np.mean(solver.d)
 
 
 if __name__ == '__main__':
     filepath = 'testcases'
     scores = []
+    data = {'N': [], 'd': []}
     for i in tqdm(range(100)):
         in_file = os.path.join(filepath, '{:04}.txt'.format(i))
         out_file = os.path.join(filepath, '{:04}_out.txt'.format(i))
-        score = run(in_file, out_file)
+        score, N, d = run(in_file, out_file)
         scores.append(score)
-        # estimate_scores.append(estimate_score)
+        data['N'].append(N)
+        data['d'].append(d)
     print('スコア平均: {} ± {}'.format(format(round(np.mean(scores)), ','), format(round(np.std(scores)), ',')))
     print('スコア: {}'.format(format(round(np.mean(scores)*50), ',')))
-    pd.DataFrame({'score': scores}).to_csv('result.csv')
+    data['score'] = scores
+    pd.DataFrame(data).to_csv('result.csv')
