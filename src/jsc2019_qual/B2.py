1cb19f7afdec54e98940360a10b8aa6d083a2a6a
Takuya Saito <takus4649@gmail.com>
2023-12-11 05:05:10 +0900

    jsc2019_qual B modify

--- src/jsc2019_qual/B2.py
+++ src/jsc2019_qual/B2.py
@@ -1,21 +1,26 @@
-# A内の転倒数×K + Aともう一つのAに対する転倒数×(1/2)(K-1)K
-
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
 mod = 10**9 + 7
-tmp1 = 0
+ans = 0
+
 for i in range(N):
+    tmp1 = 0
     for j in range(i+1, N):
         if A[i] > A[j]:
             tmp1 += 1
 
-tmp2 = 0
-for i in range(N):
-    for j in range(N):
+
+    tmp2 = 0
+
+    for j in range(i):
         if A[i] > A[j]:
             tmp2 += 1
 
-ans = tmp1*K+(tmp2)*K*(K-1)//2
-ans %= mod
+
+
+    print(i, tmp1, tmp2)
+    ans += tmp1*K+(tmp1+tmp2)*K*(K-1)//2
+    # print(ans)
+    ans %= mod
 print(ans)
