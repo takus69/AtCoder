90440f51493eff6a3135c664d6a486a19522deae
Takuya Saito <takus4649@gmail.com>
2023-12-29 06:20:02 +0900

    v7: 簡単な評価関数で実行(順位: 563, スコア: 54,784, 評価: 51,918)

---
+++ src/AHC029/simulator_eval.py
@@ -0,0 +1,16 @@
+from test_A_eval import run
+import numpy as np
+from tqdm import tqdm
+import pandas as pd
+
+
+data = {'i': [], 'score': []}
+scores = []
+for i in tqdm(range(50)):
+    score = run(i)
+    data['i'].append(i)
+    data['score'].append(score)
+    if score == 0:
+        print(f'{i} score is 0')
+print('score', format(np.sum(data['score']), ','))
+pd.DataFrame(data).to_csv('result.csv', index=False)
---
+++ src/AHC029/test_A_eval.py
@@ -0,0 +1,14 @@
+import subprocess
+
+
+def run(i):
+    output_str = subprocess.run(f'powershell cat in/{i:04}.txt | tester.exe python A_eval.py > out/{i:04}_out.txt', shell=True, capture_output=True, text=True).stderr
+    score = int(output_str.split()[-1])
+    return score
+
+
+if __name__ == '__main__':
+    i = 6
+    score = run(i)
+    print('score:', score)
+    print(subprocess.run(f'powershell cat in/{i:04}.txt | tester.exe python A_eval.py > out/{i:04}_out.txt', shell=True, capture_output=True, text=True).stderr)
