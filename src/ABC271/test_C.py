053ef47794611cd45f71b07e92898012446e8019
Takuya Saito <takus4649@gmail.com>
2024-01-04 07:56:28 +0900

    ABC271 C

---
+++ src/ABC271/test_C.py
@@ -0,0 +1,71 @@
+with open('03_rnd_01.txt') as f:
+    N = int(f.readline())
+    a = list(map(int, f.readline().split()))
+print(N)
+
+a = sorted(a)
+ans = 0
+stock = {}
+for ai in a:
+    stock[ai] = stock.get(ai, 0)+1
+keys = sorted(list(stock.keys()))
+print(len(keys), keys[:10], keys[-10:])
+print(len(stock))
+r = len(keys)-1
+stocks = 0
+for k, v in stock.items():
+    stocks += v-1
+    stock[k] = 1
+cnt = 0
+print(stock[186782], stock[186783])
+for i in range(1, N+1):
+    # print(i, stock, stocks, keys)
+    # 持っているか
+    if i in stock.keys():
+        if 186781 <= i <= 186784:
+            print(i, stock[i], stocks)
+        if stock[i] > 0:
+            cnt += 1
+            ans = i
+            stocks += stock[i]-1  # 同じ巻がある場合は売るためにストックしておく
+            stock[i] = 0
+            continue
+    # 持っていないため買う
+    sell_cnt = 2
+    # 前の巻の余りから売る
+    while sell_cnt > 0:
+        if stocks > 0:
+            stocks -= 1
+            sell_cnt -= 1
+        else:
+            break
+    if sell_cnt == 0:
+        ans = i
+        continue
+    # 足りない分は後ろの巻から売る
+    while sell_cnt > 0:
+        if stock[keys[r]] == 0:
+            r -= 1
+            if r < 0:
+                break
+            else:
+                if stock[keys[r]] == 0:
+                    break
+        if stock[keys[r]] > 0:
+            sell_cnt -= 1
+            stock[keys[r]] -= 1
+    if sell_cnt == 0:
+        ans = i
+    else:
+        break
+for k, v in stock.items():
+    if v > 0:
+        print('stock', k, v)
+print('stocks', stocks)
+print('cnt', cnt)
+cnt = 0
+for ai in set(a):
+    if ai <= 186783:
+        cnt += 1
+print('そのまま', cnt, len(a))
+print(ans)
