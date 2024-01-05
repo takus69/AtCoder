a72f0fd50daf35d3e21ccab8228cd02075605cf6
Takuya Saito <takus4649@gmail.com>
2024-01-06 05:20:24 +0900

    ABC329 F

--- src/ABC329/F.py
+++ src/ABC329/F.py
@@ -1,39 +1,18 @@
 N, Q = map(int, input().split())
 C = list(map(int, input().split()))
-C_cnt = {}
-for c in C:
-    C_cnt[c] = C_cnt.get(c, 0)+1
-box = {}  # 各箱の[cnt, l]を保持。cntは1つの箱にしか入っていない色の種類の数。lは他の箱にも同じ色が入っているものの色のリスト
-for i in range(1, N+1):
-    cnt, l = 0, []
-    c = C[i-1]
-    if C_cnt[c] > 1:
-        l = [c]
-    else:
-        cnt = 1
-    box[i] = [cnt, l]
+box = {i+1: set([C[i]]) for i in range(N)}
 
 for _ in range(Q):
     a, b = map(int, input().split())
-    a_cnt, a_l = box[a]
-    b_cnt, b_l = box[b]
-    # cnt
-    b_cnt += a_cnt
-    a_cnt = 0
-    # l
-    for c in a_l:
-        flg = False
-        for i in range(len(b_l)):
-            if b_l[i] == c:
-                flg = True
-                break
-        if flg:
-            C_cnt[c] -= 1
-            if C_cnt[c] == 1:
-                b_cnt += 1
-                del b_l[i]
-        else:
-            b_l.append(c)
-    box[a] = [0, []]
-    box[b] = [b_cnt, b_l]
-    print(b_cnt+len(b_l))
+    box_a = box[a]
+    box_b = box[b]
+    if len(box_a) <= len(box_b):
+        for c in box_a:
+            box_b.add(c)
+    else:
+        for c in box_b:
+            box_a.add(c)
+        box_b = box_a
+    box[a] = set([])
+    box[b] = box_b
+    print(len(box_b))
