0560474fa844c6876c4638b8906880110cfaa9d2
Takuya Saito <takus4649@gmail.com>
2024-03-07 04:08:56 +0900

    SegTreeのテンプレ、練習問題

--- src/practice2/J.py
+++ src/practice2/J.py
@@ -10,7 +10,7 @@ class SegTree:
         self._op = op
         self._e = e
         self._log = (n - 1).bit_length()  # 2^(_log) >= n となる最小の整数
-        self._size = 1 << self._log
+        self._size = 1 << self._log  # 2^_log
         self._d = [self._e()] * (2 * self._size)
         if v is not None:
             for i in range(self._n):
@@ -23,7 +23,7 @@ class SegTree:
         p += self._size
         self._d[p] = x
         for i in range(1, self._log + 1):
-            self._update(p >> i)
+            self._update(p >> i)  # p//2 をi回
     
     def get(self, p):
         assert 0 <= p < self._n
@@ -51,23 +51,33 @@ class SegTree:
     def _update(self, k):
         self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
 
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
 
-if __name__ == '__main__':
-    def e():
-        return 0
-    A = [1, 2, 3, 2, 1]
-    sg = SegTree(max, e, len(A), A)
-    assert sg.all_prod() == 3
-    assert sg.prod(0, 5) == 3
-    assert sg.prod(1, 3) == 3
-    assert sg.prod(1, 2) == 2
-    assert sg.prod(1, 1) == 0
-    assert sg.get(1) == 2
-    sg.set(1, 4)
-    assert sg.all_prod() == 4
-    assert sg.prod(0, 5) == 4
-    assert sg.prod(1, 3) == 4
-    assert sg.prod(1, 2) == 4
-    assert sg.prod(1, 1) == 0
-    assert sg.get(1) == 4
-    assert sg.prod(2, 5) == 3
+def e1():
+    return 0
+sg = SegTree(max, e1, N, A)
+
+ans = []
+for _ in range(Q):
+    T, q1, q2 = list(map(int, input().split()))
+    if T == 1:
+        sg.set(q1-1, q2)
+    elif T == 2:
+        ans.append(sg.prod(q1-1, q2))
+    else:
+        l, r = q1-1, N
+        if sg.prod(l, r) < q2:
+            ans.append(N+1)
+        else:
+            while l+1 < r:
+                m = (l+r)//2
+                a = sg.prod(l, m)
+                if a < q2:
+                    l = m
+                else:
+                    r = m
+            ans.append(l+1)
+
+for a in ans:
+    print(a)
